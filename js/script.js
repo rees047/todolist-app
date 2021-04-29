d784a3133f2646e0652e8560137d251dfc0aa66b
Rees <40927062+rees047@users.noreply.github.com>
2021-04-29 00:42:08 -0400

    final todolist with Jquery
    finalized todolist with Jquery implementation

--- js/script.js
+++ js/script.js
@@ -7,9 +7,29 @@ function newItem(){
     alert('You must write something!');
   }else{
     let text = $('#input').val();
-    $('#list').append('<li data-strike="crossOut"> ' + text + '</li>');
+    let li = $('<li></li>');
+    li.text(text);
+    $('#list').append(li);   
+
+    //3(i). Adding the delete button "X":
+    let deleteBtn = $('<crossOutButton></crossOutButton>');
+    deleteBtn.append(document.createTextNode('X'));
+    li.append(deleteBtn);
+
   }
 
   //2. Crossing out an item from the list of items:
+  $('#list li').off().on('dblclick', function(){ //makes sure event is only fired once
+    $(this).toggleClass('strike');
+  })
+
+  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+  $('#list li crossOutButton').off().on('click', function(){ //makes sure event is only fired once
+    $(this).parent().addClass('delete');   
+  });
+
+  // 4. Reordering the items: 
+  $('#list').sortable();   
 
 }
+
